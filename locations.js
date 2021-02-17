087a12d88484025582b32495d5ac5bf4e89a942f
Bhaskar <reddybhaskar437@gmail.com>
2021-02-17 14:16:16 -0600

    added jest and prettier packages updated index.html updated main.js added location.js

---
+++ locations.js
@@ -0,0 +1,25 @@
+export default [
+    {
+  Name: 'Horizons west apartments',
+type: 'quad',
+coordinates: [
+    {
+        latitude: 40.360857,
+        longitude: -94.888152
+    },
+    {
+        latitude: 40.360849,
+        longitude: -94.887489
+    },
+    {
+        latitude: 40.360495,
+        longitude: -94.887500
+    },
+    {
+        latitude: 40.360521,
+        longitude: -94.888210
+    }
+]
+}
+
+];
--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,28 @@
+import chooseLocation from '../locations.js';
+
 window.addEventListener('load', main);
 let synth = window.speechSynthesis;
-
+let coloringBox1 = document.getElementById("btnColor1");
+let coloringBox2 = document.getElementById("btnColor2");
+let device, location;
 
 function main() {
     console.log('Page is fully loaded');
 }
+window.addEventListener('load', main);
+coloringBox1.addEventListener('click', onClickSquareBox1);
+coloringBox1.addEventListener('touch', onClickSquareBox1);
+coloringBox2.addEventListener('click', onClickSquareBox2);
+coloringBox2.addEventListener('touch', onClickSquareBox2);
 
-async function onClickColor1() {
-    const diceValues = ["1", "2", "3", "4", "5", "6"];
-    const random = Math.floor(Math.random() * diceValues.length);
-    let diceValue = diceValues[random];
-    document.getElementById("randomValue1").innerHTML = diceValue;
-    let speech = new SpeechSynthesisUtterance(response.colorComponent1 + diceValue);
-    synth.speak(speech);
 
-}
 
-async function onClickColor2() {
-    const diceValues = ["1", "2", "3", "4", "5", "6" ];
-    const random = Math.floor(Math.random() * diceValues.length);
-    let diceValue = diceValues[random];
-    document.getElementById("randomValue2").innerHTML = diceValue;
-    let speech = new SpeechSynthesisUtterance(response.colorComponent2 + diceValue);
+
+async function  onClickSquareBox1() {
+   chooseLocation=setOfLocation[0];
+   let output = "Treasure ready: " + chooseLocation.name;
+    document.getElementById("btnColor1").innerHTML = output;
+    let speech = new SpeechSynthesisUtterance(output);
     synth.speak(speech);
     
 }
