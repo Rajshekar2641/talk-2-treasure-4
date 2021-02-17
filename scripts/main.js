bd38448fc33044ddee24ed14571c113f8e0070a5
Bhaskar <reddybhaskar437@gmail.com>
2021-02-17 16:20:47 -0600

    updated index.html locations.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,31 @@
-import chooseLocation from '../locations.js';
+import LoactionsArray from '../locations.js';
 
 window.addEventListener('load', main);
 let synth = window.speechSynthesis;
-let coloringBox1 = document.getElementById("btnColor1");
-let coloringBox2 = document.getElementById("btnColor2");
-let device, location;
+let coloringBox1 = document.getElementById("colorSelector1");
+// let coloringBox2 = document.getElementById("colorSelector2");
+
 
 function main() {
-    console.log('Page is fully loaded');
-}
+    console.log('Page is fully loaded');}
+
 window.addEventListener('load', main);
 coloringBox1.addEventListener('click', onClickSquareBox1);
 coloringBox1.addEventListener('touch', onClickSquareBox1);
-coloringBox2.addEventListener('click', onClickSquareBox2);
-coloringBox2.addEventListener('touch', onClickSquareBox2);
+// coloringBox2.addEventListener('click', onClickSquareBox2);
+// coloringBox2.addEventListener('touch', onClickSquareBox2);
+
 
 
 
 
 async function  onClickSquareBox1() {
-   chooseLocation=setOfLocation[0];
-   let output = "Treasure ready: " + chooseLocation.name;
-    document.getElementById("btnColor1").innerHTML = output;
-    let speech = new SpeechSynthesisUtterance(output);
-    synth.speak(speech);
+   location=LoactionsArray[0];
+   console.log(location);
+   let output = "Treasure ready: ";
+    document.getElementById("colorSelector1").innerHTML = output;
+    let utterance = new SpeechSynthesisUtterance(output);
+    speechSynthesis.speak(utterance);
     
 }
+
