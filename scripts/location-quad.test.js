95b356b43b1a08f4a959a2b8c179e92a6bdbba2b
GANDE RAJASHEKAR <rajshekar2641@gmail.com>
2021-02-18 17:35:22 -0600

    Added the description for test case isValidCoordinates

--- scripts/location-quad.test.js
+++ scripts/location-quad.test.js
@@ -3,8 +3,8 @@
 import isValidCoordinates from './main.js';
 
 
-//negative test case that checks when the device co-ordinates are null 
-// and location coordinates are given as undefined should throw an error.
+//negative test case that checks when the given co-ordinates are null 
+// and should return false.
 
 describe('isValidCoordinates function', () => {
     it('checks whether the coordinates are valid', () => {
